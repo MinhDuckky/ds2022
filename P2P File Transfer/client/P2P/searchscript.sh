eca673cba1ed7f679473e87103c15cb7a230788e
Dark Minh <81146050+MinhDuckky@users.noreply.github.com>
2022-02-22 12:58:56 +0700

    Group Project: Code (Group 7)

--- P2P File Transfer/client/P2P/searchscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-var=$(grep -wc "$1" filelist.txt)
-if [ $var -gt 0 ]
-then
-grep -w "$1" filelist.txt > searchresult.txt
-else
-echo "Sorry. Desired file not found" > searchresult.txt
-fi
-
